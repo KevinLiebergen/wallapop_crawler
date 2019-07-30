f2476ca30e8aaf49d235af994e0338de0c3cf7c8
KevinLiebergen <kevinliebergen@hotmail.com>
2019-07-30 21:18:26 +0200

    Primer commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Wallapop crawler
+
+Crawler creado mediante la librería Selenium de python.
+
+# Instalacion
+`$ git clone`
+<br>`$ cd `
+
+Instalación Firefox driver
+```
+$ wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
+$ tar xvfz geckodriver-v0.24.0-linux64.tar.gz
+$ sudo mv geckodriver /usr/local/bin
+$ rm geckodriver-v0.24.0-linux64.tar.gz
+```
+# Descarga librerías
+```
+$ source venv/bin/activate
+$ pip3 install -r requirements.txt
+```
+
+# Ejecución
+`(venv) $ python3 crawler.py`
---
+++ crawler_walla.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+import csv
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Diseño por http://patorjk.com/software/taag/
+
+print('''                                                                          
+ (  (          (  (                                           (           
+ )\))(   '   ) )\ )\   )                      (      ) (  (   )\  (  (    
+((_)()\ ) ( /(((_|(_| /( `  )   (  `  )     ( )(  ( /( )\))( ((_)))\ )(   
+_(())\_)())(_))_  _ )(_))/(/(   )\ /(/(     )(()\ )(_)|(_)()\ _ /((_|()\  
+\ \((_)/ ((_)_| || ((_)_((_)_\ ((_|(_)_\   ((_|(_|(_)__(()((_) (_))  ((_) 
+ \ \/\/ // _` | || / _` | '_ \) _ \ '_ \) / _| '_/ _` \ V  V / / -_)| '_| 
+  \_/\_/ \__,_|_||_\__,_| .__/\___/ .__/  \__|_| \__,_|\_/\_/|_\___||_|   
+                        |_|       |_|                                     
+      ''')
+
+
+with open('result.csv', 'w') as f:
+    f.write("Anuncios ")
+
+# Open up a Firefox browser and navigate to web page
+driver = webdriver.Firefox()
+#driver.get("https://es.wallapop.com/search?catIds=17000&kws=enduro")
+driver.get("https://www.vibbo.com/anuncios-toda-espana/motos.htm?ca=0_s&fPos=253&fOn=sb_searchtext")
+
+'''
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "myD"))
+    )
+finally:
+    driver.quit()
+'''
+
+print(driver.title)
+
+# Extract elements based on css
+titulo = driver.find_elements_by_css_selector('.product-info-title')
+precio = driver.find_elements_by_css_selector('.product-info-price')
+descripcion = driver.find_elements_by_css_selector('.product-info-description')
+
+# Print all elements
+num_page_items = len(titulo)
+
+for i in range(num_page_items):
+    print("###########################")
+    print("Titulo: " + titulo[i].text)
+    print("Precio: " + precio[i].text)
+    print("Descripcion: " + descripcion[i].text)
+
+# Close browser once task is completed
+driver.close()
