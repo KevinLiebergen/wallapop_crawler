0e41504eecbba865fe8e3eaf57d738602cec9825
KevinLiebergen <kevinliebergen@hotmail.com>
2019-08-08 18:11:16 +0200

    guardar en csv

--- crawler_walla.py
+++ crawler_walla.py
@@ -3,8 +3,10 @@ import csv
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 # Diseño por http://patorjk.com/software/taag/
+from setuptools.command.install import install
 
 print('''                                                                          
  (  (          (  (                                           (           
@@ -18,27 +20,42 @@ _(())\_)())(_))_  _ )(_))/(/(   )\ /(/(     )(()\ )(_)|(_)()\ _ /((_|()\
       ''')
 
 
+with open('resultado.csv','w') as f:
+    f.write("Titulo, precio, descripcion \n")
 
-# Open up a Firefox browser and navigate to web page
+
+
+# Abre un navegador de Firefox y navega por la pagina web
 driver = webdriver.Firefox()
-driver.get("https://es.wallapop.com/search?catIds=17000&kws=enduro")
-
+#driver.get("https://es.wallapop.com/search?catIds=17000&kws=enduro")
+driver.get("https://es.wallapop.com/coches-segunda-mano")
 
 print(driver.title)
+print("-------------------------------------")
 
-# Extract elements based on css
+
+time.sleep(10)
+
+
+# Extrae los elementos basados en los css
 titulo = driver.find_elements_by_css_selector('.product-info-title')
 precio = driver.find_elements_by_css_selector('.product-info-price')
 descripcion = driver.find_elements_by_css_selector('.product-info-description')
 
-# Print all elements
+# Imprime los elementos
 num_page_items = len(titulo)
 
 for i in range(num_page_items):
-    print("###########################")
     print("Titulo: " + titulo[i].text)
     print("Precio: " + precio[i].text)
     print("Descripcion: " + descripcion[i].text)
+    print("###########################")
 
-# Close browser once task is completed
+'''
+with open('resultado.csv','a') as f:
+    for i in range(num_page_items):
+        f.write(titulo[i].text + "," + precio[i].text + ", " + descripcion[i].text + "\n")
+'''
+
+# Cierra el navegador
 driver.close()
