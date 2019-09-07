4e55313ffa2e3bf24486955cacbc36caa3b6be71
KevinLiebergen <kevinliebergen@hotmail.com>
2019-09-07 11:22:25 +0200

    Añadido clase informacion pantalla para outputs

---
+++ dockerfile_dir/outputs/informacion_pantalla.py
@@ -0,0 +1,24 @@
+
+class InformacionPantalla:
+    def __init__(self, producto):
+        self.titulo = producto["titulo"]
+        self.precio = producto["precio"]
+        self.descripcion = producto["descripcion"]
+        self.barrio = producto["barrio"]
+        self.ciudad = producto["ciudad"]
+        self.fecha_publicacion = producto["fechaPublicacion"]
+        self.puntuacion_vendedor = producto["puntuacion"]
+        self.imagen = producto["imagenURL"]
+        self.url = producto["url"]
+
+    def imprimir_elementos(self):
+        print("Titulo: " + self.titulo)
+        print("Precio: " + self.precio)
+        print("Descripcion: " + self.descripcion)
+        print("Barrio: " + self.barrio)
+        print("Ciudad: " + self.ciudad)
+        print("Fecha publicacion: " + self.fecha_publicacion)
+        print("Puntuacion vendedor: " + self.puntuacion_vendedor)
+        print("Imagen: " + self.imagen)
+        print("URL: " + self.url)
+        print("###########################")
